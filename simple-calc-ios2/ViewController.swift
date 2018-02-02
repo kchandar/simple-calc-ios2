26bcb053cea2587973e3654cbea78413472f392a
Keertana Chandar <kchandar@uw.edu>
2018-02-02 00:18:54 -0800

    Initial Commit

---
+++ simple-calc-ios2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simple-calc-ios2
+//
+//  Created by Keertana Chandar on 2/2/18.
+//  Copyright © 2018 Keertana Chandar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
